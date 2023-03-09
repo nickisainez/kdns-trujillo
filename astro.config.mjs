e26b654decd130487f47199a0b17e98abf364cb0
nickisainez <nickiaasainez@gmail.com>
2023-03-08 23:33:01 -0500

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,14 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
-
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    react(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), react()],
+  output: "server",
+  adapter: vercel()
 });
